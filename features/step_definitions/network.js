e41ddaf78c00f7076da00770e99b70785845f801
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-03-06 15:06:11 +0100

    make network steps agnostic

--- features/step_definitions/network.js
+++ features/step_definitions/network.js
@@ -4,22 +4,22 @@ const { Given, When, Then } = require('cucumber');
 const { expect } = require('chai');
 
 Given('the South America Network', function() {
-  this.southAmericaNetwork = new Network();
-  this.southAmericaNetwork.addLink('Lima', 'Santiago');
-  this.southAmericaNetwork.addLink('Lima', 'Bogota');
-  this.southAmericaNetwork.addLink('Lima', 'Mexico');
-  this.southAmericaNetwork.addLink('Bogota', 'Mexico');
-  this.southAmericaNetwork.addLink('Bogota', 'Lima');
-  this.southAmericaNetwork.addLink('Santiago', 'Lima');
-  this.southAmericaNetwork.addLink('Mexico', 'Lima');
-  this.southAmericaNetwork.addLink('Mexico', 'Bogota');
+  this.network = new Network();
+  this.network.addLink('Lima', 'Santiago');
+  this.network.addLink('Lima', 'Bogota');
+  this.network.addLink('Lima', 'Mexico');
+  this.network.addLink('Bogota', 'Mexico');
+  this.network.addLink('Bogota', 'Lima');
+  this.network.addLink('Santiago', 'Lima');
+  this.network.addLink('Mexico', 'Lima');
+  this.network.addLink('Mexico', 'Bogota');
 });
 
 Then('the South America Network should have the links:', function(dataTable) {
   for (row of dataTable.rows()) {
     const [city, connectedCities] = row;
     for (connectedCity of connectedCities.split(', ')) {
-      expect(this.southAmericaNetwork.isLinked(city, connectedCity)).to.be.true;
+      expect(this.network.isLinked(city, connectedCity)).to.be.true;
     }
   }
 });
