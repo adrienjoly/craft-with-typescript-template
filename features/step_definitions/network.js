28aa06fd14e62eca26531e48f8e54e71b821a445
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-03-06 15:14:25 +0100

    refacto: Network::addLink() -> addCity()

--- features/step_definitions/network.js
+++ features/step_definitions/network.js
@@ -5,20 +5,16 @@ const { expect } = require('chai');
 
 Given('the South America Network', function() {
   this.network = new Network();
-  this.network.addLink('Lima', 'Santiago');
-  this.network.addLink('Lima', 'Bogota');
-  this.network.addLink('Lima', 'Mexico');
-  this.network.addLink('Bogota', 'Mexico');
-  this.network.addLink('Bogota', 'Lima');
-  this.network.addLink('Santiago', 'Lima');
-  this.network.addLink('Mexico', 'Lima');
-  this.network.addLink('Mexico', 'Bogota');
+  this.network.addCity('Lima', ['Santiago', 'Bogota', 'Mexico']);
+  this.network.addCity('Bogota', ['Mexico', 'Lima']);
+  this.network.addCity('Santiago', ['Lima']);
+  this.network.addCity('Mexico', ['Lima', 'Bogota']);
 });
 
 Then('the South America Network should have the links:', function(dataTable) {
   for (row of dataTable.rows()) {
     const [city, connectedCities] = row;
-    for (connectedCity of connectedCities.split(', ')) {
+    for (const connectedCity of connectedCities.split(', ')) {
       expect(this.network.isLinked(city, connectedCity)).to.be.true;
     }
   }
--- src/Network.js
+++ src/Network.js
@@ -4,8 +4,12 @@ class Network {
   constructor() {
     this.citiesLinkedTo = {};
   }
-  addLink(city1, city2) {
-    (this.citiesLinkedTo[city1] = this.citiesLinkedTo[city1] || []).push(city2);
+  addCity(city, connectedCities) {
+    for (const connectedCity of connectedCities) {
+      (this.citiesLinkedTo[city] = this.citiesLinkedTo[city] || []).push(
+        connectedCity
+      );
+    }
   }
   isLinked(city1, city2) {
     return (this.citiesLinkedTo[city1] || []).includes(city2);
