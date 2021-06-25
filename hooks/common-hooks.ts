6533e2257004f597dcd8a6c67baf578a6f01dba0
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-06-25 11:37:24 +0200

    clean up hooks

--- hooks/common-hooks.ts
+++ hooks/common-hooks.ts
@@ -1,21 +1,20 @@
-import { CustomWorld } from '../world';
 import { Before, BeforeAll, AfterAll } from '@cucumber/cucumber';
 
-Before({ tags: '@ignore' }, async function () {
+Before({ tags: '@ignore' }, async function() {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return 'skipped' as any;
 });
 
-Before({ tags: '@debug' }, async function (this: CustomWorld) {
+Before({ tags: '@debug' }, async function(this: { debug: boolean }) {
   this.debug = true;
 });
 
-BeforeAll(async function () {
+BeforeAll(async function() {
   // eslint-disable-next-line no-console
   console.log('Before All');
 });
 
-AfterAll(async function () {
+AfterAll(async function() {
   // eslint-disable-next-line no-console
   console.log('After All');
 });
