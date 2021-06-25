ef3c8f18cbf99e1a3a5f11437a2489871407ce0d
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-06-25 11:52:44 +0200

    fix linter issues (#8)
    * display colors in eslint logs
    
    * fix github actions ci
    
    * remove mention to node version matrix
    
    * make vscode display all eslint errors and warnings => reuse eslint config file from https://github.com/hdorgeval/cucumber7-ts-starter
    
    * fix linter issues

--- step-definitions/network.ts
+++ step-definitions/network.ts
@@ -1,10 +1,10 @@
-const { Network } = require('../src/Network');
+import { Network } from '../src/Network';
 
-const { Given, When, Then, DataTable } = require('@cucumber/cucumber');
-const { expect } = require('chai');
+import { Given, Then, DataTable } from '@cucumber/cucumber';
+import { expect } from 'chai';
 
 type Context = {
-  network: typeof Network;
+  network: Network;
 };
 
 Given('the South America Network', function(this: Context) {
@@ -17,7 +17,7 @@ Given('the South America Network', function(this: Context) {
 
 Then('the South America Network should have the links:', function(
   this: Context,
-  dataTable: typeof DataTable
+  dataTable: DataTable
 ) {
   for (const row of dataTable.rows()) {
     const [city, connectedCities] = row;
